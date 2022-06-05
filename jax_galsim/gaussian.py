dfccb0c3646bc3713798982b0a4821591ab44d76
EiffL <fr.eiffel@gmail.com>
2022-06-05 15:05:50 +0200

    adds minimal implem of gsobject

---
+++ jax_galsim/gaussian.py
@@ -0,0 +1,123 @@
+import jax.numpy as jnp
+
+from jax_galsim.gsobject import GSObject
+from jax_galsim.gsparams import GSParams
+
+import galsim as _galsim
+from jax._src.numpy.util import _wraps
+from jax.tree_util import register_pytree_node_class
+
+
+@_wraps(_galsim.Gaussian)
+@register_pytree_node_class
+class Gaussian(GSObject):
+    # The FWHM of a Gaussian is 2 sqrt(2 ln2) sigma
+    _fwhm_factor = 2.3548200450309493
+    # The half-light-radius is sqrt(2 ln2) sigma
+    _hlr_factor = 1.1774100225154747
+    # 1/(2pi)
+    _inv_twopi = 0.15915494309189535
+
+    _has_hard_edges = False
+    _is_axisymmetric = True
+    _is_analytic_x = True
+    _is_analytic_k = True
+
+    def __init__(self, half_light_radius=None, sigma=None, fwhm=None, flux=1., gsparams=None):
+
+        # Checking gsparams
+        gsparams = GSParams.check(gsparams)
+
+        if fwhm is not None:
+            if sigma is not None or half_light_radius is not None:
+                raise _galsim.GalSimIncompatibleValuesError(
+                    "Only one of sigma, fwhm, and half_light_radius may be specified",
+                    fwhm=fwhm, sigma=sigma, half_light_radius=half_light_radius)
+            else:
+                super().__init__(fwhm=fwhm, flux=flux, gsparams=gsparams)
+        elif half_light_radius is not None:
+            if sigma is not None:
+                raise _galsim.GalSimIncompatibleValuesError(
+                    "Only one of sigma, fwhm, and half_light_radius may be specified",
+                    fwhm=fwhm, sigma=sigma, half_light_radius=half_light_radius)
+            else:
+                super().__init__(half_light_radius=half_light_radius, flux=flux, gsparams=gsparams)
+        elif sigma is None:
+            raise _galsim.GalSimIncompatibleValuesError(
+                "One of sigma, fwhm, and half_light_radius must be specified",
+                fwhm=fwhm, sigma=sigma, half_light_radius=half_light_radius)
+        else:
+            super().__init__(sigma=sigma, flux=flux, gsparams=gsparams)
+
+        self._sigsq = self.sigma**2
+        self._inv_sigsq = 1./self._sigsq
+        self._norm = self.flux * self._inv_sigsq * Gaussian._inv_twopi
+
+    @property
+    def sigma(self):
+        """The sigma of this Gaussian profile
+        """
+        if 'fwhm' in self.params:
+            return self.params['fwhm'] / Gaussian._fwhm_factor
+        elif 'half_light_radius' in self.params:
+            return self.params['half_light_radius'] / Gaussian._fwhm_factor
+        else:
+            return self.params['sigma']
+
+    @property
+    def half_light_radius(self):
+        """The half-light radius of this Gaussian profile
+        """
+        return self.sigma * Gaussian._hlr_factor
+
+    @property
+    def fwhm(self):
+        """The FWHM of this Gaussian profile
+        """
+        return self.sigma * Gaussian._fwhm_factor
+
+    def __eq__(self, other):
+        return (self is other or
+                (isinstance(other, Gaussian) and
+                 self.sigma == other.sigma and
+                 self.flux == other.flux and
+                 self.gsparams == other.gsparams))
+
+    def __hash__(self):
+        return hash(("galsim.Gaussian", self.sigma, self.flux, self.gsparams))
+
+    def __repr__(self):
+        return 'galsim.Gaussian(sigma=%r, flux=%r, gsparams=%r)' % (
+            self.sigma, self.flux, self.gsparams)
+
+    def __str__(self):
+        s = 'galsim.Gaussian(sigma=%s' % self.sigma
+        if self.flux != 1.0:
+            s += ', flux=%s' % self.flux
+        s += ')'
+        return s
+
+    @property
+    def _maxk(self):
+        return jnp.sqrt(-2.*jnp.log(self.gsparams.maxk_threshold))/self.sigma
+
+    @property
+    def _stepk(self):
+        R = jnp.array([jnp.sqrt(-2.*jnp.log(self.gsparams.folding_threshold)),
+                       self.gsparams.stepk_minimum_hlr * Gaussian._hlr_factor]) .max()
+        return jnp.pi / (R * self.sigma)
+
+    @ property
+    def _max_sb(self):
+        return self._norm
+
+    def _xValue(self, pos):
+        rsq = pos.x**2 + pos.y**2
+        return self._norm * jnp.exp(-0.5 * rsq * self._inv_sigsq)
+
+    def _kValue(self, kpos):
+        ksq = (kpos.x**2 + kpos.y**2) * self._sigsq
+        return self.flux * jnp.exp(-0.5 * ksq)
+
+    def withFlux(self, flux):
+        return Gaussian(sigma=self.sigma, flux=flux, gsparams=self.gsparams)
---
+++ jax_galsim/sum.py
@@ -0,0 +1,174 @@
+import numpy as np
+import jax.numpy as jnp
+
+from jax_galsim.gsparams import GSParams
+from jax_galsim.gsobject import GSObject
+
+import galsim as _galsim
+from jax._src.numpy.util import _wraps
+from jax.tree_util import register_pytree_node_class
+
+
+@_wraps(_galsim.Add, lax_description="Does not support `ChromaticObject` at this point.")
+def Add(*args, **kwargs):
+    return Sum(*args, **kwargs)
+
+
+@_wraps(_galsim.Sum, lax_description="Does not support `ChromaticObject` at this point.")
+@register_pytree_node_class
+class Sum(GSObject):
+
+    def __init__(self, *args, gsparams=None, propagate_gsparams=True):
+
+        self._propagate_gsparams = propagate_gsparams
+
+        if len(args) == 0:
+            raise TypeError(
+                "At least one GSObject must be provided.")
+        elif len(args) == 1:
+            # 1 argument.  Should be either a GSObject or a list of GSObjects
+            if isinstance(args[0], GSObject):
+                args = [args[0]]
+            elif isinstance(args[0], list) or isinstance(args[0], tuple):
+                args = args[0]
+            else:
+                raise TypeError(
+                    "Single input argument must be a GSObject or list of them.")
+        # else args is already the list of objects
+
+        # Consolidate args for Sums of Sums...
+        new_args = []
+        for a in args:
+            if isinstance(a, Sum):
+                new_args.extend(a.params['obj_list'])
+            else:
+                new_args.append(a)
+        args = new_args
+
+        for obj in args:
+            if not isinstance(obj, GSObject):
+                raise TypeError(
+                    "Arguments to Sum must be GSObjects, not %s" % obj)
+
+        # Figure out what gsparams to use
+        if gsparams is None:
+            # If none is given, take the most restrictive combination from the obj_list.
+            self._gsparams = GSParams.combine([obj.gsparams for obj in args])
+        else:
+            # If something explicitly given, then use that.
+            self._gsparams = GSParams.check(gsparams)
+
+        # Apply gsparams to all in obj_list.
+        if self._propagate_gsparams:
+            args = [obj.withGSParams(self._gsparams) for obj in args]
+
+        # Save the list as an attribute, so it can be inspected later if necessary.
+        self._params = {'obj_list': args}
+
+    @property
+    def obj_list(self):
+        """The list of objects being added.
+        """
+        return self._params['obj_list']
+
+    @property
+    def flux(self):
+        flux_list = jnp.array([obj.flux for obj in self.obj_list])
+        return jnp.sum(flux_list)
+
+    def withGSParams(self, gsparams=None, **kwargs):
+        """Create a version of the current object with the given gsparams
+
+        .. note::
+
+            Unless you set ``propagate_gsparams=False``, this method will also update the gsparams
+            of each object being added.
+        """
+        if gsparams == self.gsparams:
+            return self
+        ret = self.__class__(
+            self.params['obj_list'], gsparams=gsparams, propagate_gsparams=self._propagate_gsparams)
+        return ret
+
+    def __eq__(self, other):
+        return (self is other or
+                (isinstance(other, Sum) and
+                 self.obj_list == other.obj_list and
+                 self.gsparams == other.gsparams and
+                 self._propagate_gsparams == other._propagate_gsparams))
+
+    def __hash__(self):
+        return hash(("galsim.Sum", tuple(self.obj_list), self.gsparams, self._propagate_gsparams))
+
+    def __repr__(self):
+        return 'galsim.Sum(%r, gsparams=%r, propagate_gsparams=%r)' % (self.obj_list, self.gsparams,
+                                                                       self._propagate_gsparams)
+
+    def __str__(self):
+        str_list = [str(obj) for obj in self.obj_list]
+        return '(' + ' + '.join(str_list) + ')'
+
+    @property
+    def _maxk(self):
+        maxk_list = jnp.array([obj.maxk for obj in self.obj_list])
+        return jnp.max(maxk_list)
+
+    @property
+    def _stepk(self):
+        stepk_list = jnp.array([obj.stepk for obj in self.obj_list])
+        return jnp.min(stepk_list)
+
+    @property
+    def _has_hard_edges(self):
+        hard_list = [obj.has_hard_edges for obj in self.obj_list]
+        return bool(np.any(hard_list))
+
+    @property
+    def _is_axisymmetric(self):
+        axi_list = [obj.is_axisymmetric for obj in self.obj_list]
+        return bool(np.all(axi_list))
+
+    @property
+    def _is_analytic_x(self):
+        ax_list = [obj.is_analytic_x for obj in self.obj_list]
+        return bool(np.all(ax_list))
+
+    @property
+    def _is_analytic_k(self):
+        ak_list = [obj.is_analytic_k for obj in self.obj_list]
+        return bool(np.all(ak_list))
+
+    @property
+    def _centroid(self):
+        cen_list = jnp.array(
+            [obj.centroid * obj.flux for obj in self.obj_list])
+        return jnp.sum(cen_list) / self.flux
+
+    @property
+    def _max_sb(self):
+        sb_list = jnp.array([obj.max_sb for obj in self.obj_list])
+        return jnp.sum(sb_list)
+
+    def _xValue(self, pos):
+        xv_list = jnp.array([obj._xValue(pos) for obj in self.obj_list])
+        return jnp.sum(xv_list, axis=0)
+
+    def _kValue(self, pos):
+        kv_list = jnp.array([obj._kValue(pos) for obj in self.obj_list])
+        return jnp.sum(kv_list, axis=0)
+
+    def tree_flatten(self):
+        """This function flattens the GSObject into a list of children
+        nodes that will be traced by JAX and auxiliary static data."""
+        # Define the children nodes of the PyTree that need tracing
+        children = (self.params, )
+        # Define auxiliary static data that doesn’t need to be traced
+        aux_data = {'gsparams': self.gsparams,
+                    'propagate_gs_params': self._propagate_gsparams}
+        return (children, aux_data)
+
+    @classmethod
+    def tree_unflatten(cls, aux_data, children):
+        """ Recreates an instance of the class from flatten representation
+        """
+        return cls(children[0]["obj_list"], **aux_data)
