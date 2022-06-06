aceb2cfffd84d76f4d3592b3e36588d4fdba313d
EiffL <fr.eiffel@gmail.com>
2022-06-06 02:56:51 +0200

    adds minimal setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='JAX-GalSim',
+    version='0.0.1rc1',
+    url='https://github.com/GalSim-developers/JAX-GalSim',
+    author="GalSim Developers",
+    description='The modular galaxy image simulation toolkit, but in JAX',
+    packages=find_packages(),
+    license="BSD License",
+    install_requires=['numpy >= 1.18.0', 'jax >= 0.3.0', 'galsim'],
+)
