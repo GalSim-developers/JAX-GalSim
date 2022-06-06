243c04a0af82bb4cc76da5f7c135cf32b6ec4376
EiffL <fr.eiffel@gmail.com>
2022-06-07 01:11:03 +0200

    adds position implementation

--- jax_galsim/__init__.py
+++ jax_galsim/__init__.py
@@ -2,8 +2,12 @@ from jax.config import config
 
 config.update("jax_enable_x64", True)
 
+# First some basic building blocks that don't usually depend on anything else
+from jax_galsim.position import Position, PositionD, PositionI
+
+
+from jax_galsim.gsparams import GSParams
+from jax_galsim.gsobject import GSObject
+from jax_galsim.sum import Add, Sum
 from jax_galsim.exponential import Exponential
 from jax_galsim.gaussian import Gaussian
-from jax_galsim.sum import Add, Sum
-from jax_galsim.gsobject import GSObject
-from jax_galsim.gsparams import GSParams
---
+++ jax_galsim/position.py
@@ -0,0 +1,147 @@
+import jax.numpy as jnp
+
+from jax._src.numpy.util import _wraps
+import galsim as _galsim
+
+
+@_wraps(_galsim.Position)
+class Position(object):
+    def __init__(self, *args, **kwargs):
+        if len(kwargs) == 0:
+            if len(args) == 2:
+                self.x, self.y = args
+            elif len(args) == 0:
+                self.x = self.y = 0
+            elif len(args) == 1:
+                if isinstance(args[0], (Position,)):
+                    self.x = args[0].x
+                    self.y = args[0].y
+                else:
+                    try:
+                        self.x, self.y = args[0]
+                    except (TypeError, ValueError):
+                        raise TypeError(
+                            "Single argument to %s must be either a Position "
+                            "or a tuple." % self.__class__
+                        )
+            else:
+                raise TypeError(
+                    "%s takes at most 2 arguments (%d given)"
+                    % (self.__class__, len(args))
+                )
+        elif len(args) != 0:
+            raise TypeError(
+                "%s takes x and y as either named or unnamed arguments (given %s, %s)"
+                % (self.__class__, args, kwargs)
+            )
+        else:
+            try:
+                self.x = kwargs.pop("x")
+                self.y = kwargs.pop("y")
+            except KeyError:
+                raise TypeError(
+                    "Keyword arguments x,y are required for %s" % self.__class__
+                )
+            if kwargs:
+                raise TypeError("Got unexpected keyword arguments %s" % kwargs.keys())
+
+        # Make sure the inputs are indeed jax numpy values
+        self.x = jnp.asarray(self.x)
+        self.y = jnp.asarray(self.y)
+
+    def __mul__(self, other):
+        return self.__class__(self.x * other, self.y * other)
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    def __div__(self, other):
+        return self.__class__(self.x / other, self.y / other)
+
+    __truediv__ = __div__
+
+    def __neg__(self):
+        return self.__class__(-self.x, -self.y)
+
+    def __add__(self, other):
+        # from .bounds import Bounds
+        # if isinstance(other,Bounds):
+        #     return other + self
+        if not isinstance(other, Position):
+            raise TypeError("Can only add a Position to a %s" % self.__class__.__name__)
+        elif isinstance(other, self.__class__):
+            return self.__class__(self.x + other.x, self.y + other.y)
+        else:
+            return PositionD(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        if not isinstance(other, Position):
+            raise TypeError(
+                "Can only subtract a Position from a %s" % self.__class__.__name__
+            )
+        elif isinstance(other, self.__class__):
+            return self.__class__(self.x - other.x, self.y - other.y)
+        else:
+            return PositionD(self.x - other.x, self.y - other.y)
+
+    def __repr__(self):
+        return "galsim.%s(x=%r, y=%r)" % (self.__class__.__name__, self.x, self.y)
+
+    def __str__(self):
+        return "galsim.%s(%s,%s)" % (self.__class__.__name__, self.x, self.y)
+
+    def __eq__(self, other):
+        return self is other or (
+            isinstance(other, self.__class__)
+            and self.x == other.x
+            and self.y == other.y
+        )
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __hash__(self):
+        return hash((self.__class__.__name__, self.x, self.y))
+
+    def shear(self, shear):
+        """Shear the position.
+
+        See the doc string of `galsim.Shear.getMatrix` for more details.
+
+        Parameters:
+            shear:    a `galsim.Shear` instance
+
+        Returns:
+            a `galsim.PositionD` instance.
+        """
+        shear_mat = shear.getMatrix()
+        shear_pos = jnp.dot(shear_mat, jnp.stack([self.x, self.y], axis=0))
+        return PositionD(shear_pos[0], shear_pos[1])
+
+    def tree_flatten(self):
+        """This function flattens the GSObject into a list of children
+        nodes that will be traced by JAX and auxiliary static data."""
+        # Define the children nodes of the PyTree that need tracing
+        children = (self.x, self.y)
+        return (children, None)
+
+    @classmethod
+    def tree_unflatten(cls, aux_data, children):
+        """Recreates an instance of the class from flatten representation"""
+        return cls(x=children[0], y=children[1])
+
+
+@_wraps(_galsim.PositionD)
+class PositionD(Position):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.x = self.x.astype("float")
+        self.y = self.y.astype("float")
+
+
+@_wraps(_galsim.PositionI)
+class PositionI(Position):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.x = self.x.astype("int")
+        self.y = self.y.astype("int")
