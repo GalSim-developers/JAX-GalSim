798968759304bd24fbcf6caaaa21fbba8104d9cb
Ismael Mendoza <11745764+ismael-mendoza@users.noreply.github.com>
2023-07-04 12:24:30 +0000

    fix exponential black

--- jax_galsim/exponential.py
+++ jax_galsim/exponential.py
@@ -46,12 +46,6 @@ class Exponential(GSObject):
         else:
             super().__init__(scale_radius=scale_radius, flux=flux, gsparams=gsparams)
 
-        #Transfert to @property
-        #self._r0 = self.scale_radius
-        #self._inv_r0 = 1.0 / self._r0
-        #self._norm = self.flux * Exponential._inv_twopi * self._inv_r0**2
-
-
     @property
     def scale_radius(self):
         """The scale radius of the profile."""
@@ -59,6 +53,7 @@ class Exponential(GSObject):
             return self.params["half_light_radius"] / Exponential._hlr_factor
         else:
             return self.params["scale_radius"]
+
     @property
     def _r0(self):
         return self.scale_radius
@@ -70,7 +65,6 @@ class Exponential(GSObject):
     @property
     def _norm(self):
         return self.flux * Exponential._inv_twopi * self._inv_r0**2
-    
 
     @property
     def half_light_radius(self):
@@ -79,7 +73,6 @@ class Exponential(GSObject):
             return self.params["half_light_radius"]
         else:
             return self.params["scale_radius"] * Exponential._hlr_factor
-
 
     def __hash__(self):
         return hash(("galsim.Exponential", self.scale_radius, self.flux, self.gsparams))
