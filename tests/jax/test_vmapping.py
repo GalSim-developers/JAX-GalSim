35808613e2f09f714e7ea0e2bd770543648fba02
Ismael Mendoza <11745764+ismael-mendoza@users.noreply.github.com>
2023-07-04 12:58:32 +0000

    newline and black

--- tests/jax/test_vmapping.py
+++ tests/jax/test_vmapping.py
@@ -1,13 +1,14 @@
 import jax
-import jax_galsim as galsim
-
 import jax.numpy as jnp
 
+import jax_galsim as galsim
 
 e = jnp.ones(2)
 
+
 def duplicate(params):
     return {x: y * e for x, y in params.items()}
+
 
 def test_gaussian_vmapping():
     # Test Gaussian objects
@@ -24,9 +25,7 @@ def test_gaussian_vmapping():
         ).all()
 
     # Check that after vmapping the oject is still the same
-    assert all(
-        [test_eq(jax.vmap(galsim.Gaussian)(**duplicate(o.params)), o) for o in objects]
-    )
+    assert all([test_eq(jax.vmap(galsim.Gaussian)(**duplicate(o.params)), o) for o in objects])
 
 
 def test_exponential_vmapping():
@@ -43,18 +42,14 @@ def test_exponential_vmapping():
         ).all() and (self.flux == jnp.array([other.flux, other.flux])).all()
 
     # Check that after vmapping the oject is still the same
-    assert all(
-        [
-            test_eq(jax.vmap(galsim.Exponential)(**duplicate(o.params)), o)
-            for o in objects
-        ]
-    )
+    assert all([test_eq(jax.vmap(galsim.Exponential)(**duplicate(o.params)), o) for o in objects])
 
 
 def eq_pos(pos, other):
-        return (pos.x == jnp.array([other.x, other.x])).all() and (
-            pos.y == jnp.array([other.y, other.y])
-        ).all()
+    return (pos.x == jnp.array([other.x, other.x])).all() and (
+        pos.y == jnp.array([other.y, other.y])
+    ).all()
+
 
 def test_position_vmapping():
     obj = galsim.PositionD(1.0, 2.0)
@@ -72,20 +67,22 @@ def test_affine_transform_vmapping():
         world_origin=galsim.PositionD(1.0, 2.0),
     )
 
-    print('now vmap')
+    print("now vmap")
 
     obj_duplicated = jax.vmap(galsim.AffineTransform)(
-        1.0*e,
-        0.0*e,
-        0.0*e,
-        1.0*e,
-        origin=jax.vmap(galsim.PositionD)(1.0*e, 2.0*e),
-        world_origin=jax.vmap(galsim.PositionD)(1.0*e, 2.0*e),
+        1.0 * e,
+        0.0 * e,
+        0.0 * e,
+        1.0 * e,
+        origin=jax.vmap(galsim.PositionD)(1.0 * e, 2.0 * e),
+        world_origin=jax.vmap(galsim.PositionD)(1.0 * e, 2.0 * e),
     )
 
     def test_eq(self, other):
         return (
-            (self._local_wcs.dudx == jnp.array([other._local_wcs.dudx, other._local_wcs.dudx])).all()
+            (
+                self._local_wcs.dudx == jnp.array([other._local_wcs.dudx, other._local_wcs.dudx])
+            ).all()
             and eq_pos(self.origin, other.origin)
             and eq_pos(self.world_origin, other.world_origin)
         )
@@ -95,17 +92,18 @@ def test_affine_transform_vmapping():
 
 def test_bounds_vmapping():
     obj = galsim.BoundsD(0.0, 1.0, 0.0, 1.0)
-    obj_d = jax.vmap(galsim.BoundsD)(0.0*e, 1.0*e, 0.0*e, 1.0*e)
+    obj_d = jax.vmap(galsim.BoundsD)(0.0 * e, 1.0 * e, 0.0 * e, 1.0 * e)
 
     objI = galsim.BoundsI(0.0, 1.0, 0.0, 1.0)
-    objI_d = jax.vmap(galsim.BoundsI)(0.0*e, 1.0*e, 0.0*e, 1.0*e)
+    objI_d = jax.vmap(galsim.BoundsI)(0.0 * e, 1.0 * e, 0.0 * e, 1.0 * e)
 
     def test_eq(self, other):
-      return (self.xmin == jnp.array([other.xmin, other.xmin])
-      ).all() and (self.xmax == jnp.array([other.xmax, other.xmax])
-      ).all() and (self.ymin == jnp.array([other.ymin, other.ymin])
-      ).all() and (self.ymax == jnp.array([other.ymax, other.ymax])).all()
-      
+        return (
+            (self.xmin == jnp.array([other.xmin, other.xmin])).all()
+            and (self.xmax == jnp.array([other.xmax, other.xmax])).all()
+            and (self.ymin == jnp.array([other.ymin, other.ymin])).all()
+            and (self.ymax == jnp.array([other.ymax, other.ymax])).all()
+        )
 
     assert test_eq(obj_d, obj)
     assert test_eq(objI_d, objI)
