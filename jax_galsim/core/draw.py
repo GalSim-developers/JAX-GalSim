5ae0d7c7e870f7a736db01c9c2853a0d4e453441
Ismael Mendoza <11745764+ismael-mendoza@users.noreply.github.com>
2023-07-20 00:06:20 +0000

    fix draw function

--- jax_galsim/core/draw.py
+++ jax_galsim/core/draw.py
@@ -30,4 +30,4 @@ def draw_by_xValue(gsobject, image, jacobian=jnp.eye(2), offset=jnp.zeros(2), fl
     im = (im * flux_scaling).astype(image.dtype)
 
     # Return an image
-    return Image.init(array=im, bounds=image.bounds, wcs=image.wcs)
+    return Image(_array=im, _bounds=image.bounds, wcs=image.wcs, _dtype=image.dtype)
