c1933f1f021a2bae9d082dc6a426b9de39b1faf5
Ismael Mendoza <11745764+ismael-mendoza@users.noreply.github.com>
2023-08-07 13:33:24 +0000

    reverte image to main

--- jax_galsim/image.py
+++ jax_galsim/image.py
@@ -1,14 +1,15 @@
-import galsim as _galsim
 import jax
 import jax.numpy as jnp
 import numpy as np
+
+import galsim as _galsim
 from jax._src.numpy.util import _wraps
 from jax.tree_util import register_pytree_node_class
 
-from jax_galsim.bounds import BoundsD, BoundsI
-from jax_galsim.position import PositionI
+from jax_galsim.position import PositionI, PositionD
+from jax_galsim.bounds import BoundsI, BoundsD
+from jax_galsim.wcs import BaseWCS, PixelScale, JacobianWCS
 from jax_galsim.utilities import parse_pos_args
-from jax_galsim.wcs import BaseWCS, PixelScale
 
 
 @_wraps(
