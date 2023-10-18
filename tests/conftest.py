29876bbae2dedb94af2fc4373df266a1d3b3fb29
beckermr <becker.mr@gmail.com>
2023-10-18 17:13:10 -0500

    blacken

--- tests/conftest.py
+++ tests/conftest.py
@@ -24,9 +24,7 @@ def pytest_ignore_collect(collection_path, path, config):
     These somtimes fail to import and cause pytest to fail.
     """
     if "tests/GalSim/tests" in str(collection_path):
-        if not any(
-            [t in str(collection_path) for t in test_config["enabled_tests"]]
-        ):
+        if not any([t in str(collection_path) for t in test_config["enabled_tests"]]):
             return True
 
 
