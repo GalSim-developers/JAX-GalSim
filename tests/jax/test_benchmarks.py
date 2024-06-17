d07ad50b40693d911fb883875fab3bd3bb0a8067
beckermr <becker.mr@gmail.com>
2024-06-17 12:39:54 -0500

    fix: blacken

--- tests/jax/test_benchmarks.py
+++ tests/jax/test_benchmarks.py
@@ -6,7 +6,9 @@ import jax_galsim as jgs
 def test_benchmarks_interpolated_image_jit_compile(benchmark):
     gal = jgs.Gaussian(fwhm=1.2)
     im_gal = gal.drawImage(nx=32, ny=32, scale=0.2)
-    igal = jgs.InterpolatedImage(im_gal, gsparams=jgs.GSParams(minimum_fft_size=128, maximum_fft_size=128))
+    igal = jgs.InterpolatedImage(
+        im_gal, gsparams=jgs.GSParams(minimum_fft_size=128, maximum_fft_size=128)
+    )
 
     def f():
         igal.drawImage(nx=32, ny=32, scale=0.2)
@@ -17,7 +19,9 @@ def test_benchmarks_interpolated_image_jit_compile(benchmark):
 def test_benchmarks_interpolated_image_jit_run(benchmark):
     gal = jgs.Gaussian(fwhm=1.2)
     im_gal = gal.drawImage(nx=32, ny=32, scale=0.2)
-    igal = jgs.InterpolatedImage(im_gal, gsparams=jgs.GSParams(minimum_fft_size=128, maximum_fft_size=128))
+    igal = jgs.InterpolatedImage(
+        im_gal, gsparams=jgs.GSParams(minimum_fft_size=128, maximum_fft_size=128)
+    )
 
     def f():
         igal.drawImage(nx=32, ny=32, scale=0.2)
