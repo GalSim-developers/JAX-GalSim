2cbcb5eb68cf3f6a2b875144aebad005fa343e15
beckermr <becker.mr@gmail.com>
2024-06-21 14:08:08 -0500

    feat: add benchmarks for metacal and cleanup code

--- tests/jax/test_benchmarks.py
+++ tests/jax/test_benchmarks.py
@@ -1,22 +1,16 @@
+from functools import partial
+
+import galsim as _galsim
 import jax
+import jax.numpy as jnp
+import numpy as np
+import pytest
 
 import jax_galsim as jgs
 
 
-def test_benchmarks_interpolated_image_jit_compile(benchmark):
-    gal = jgs.Gaussian(fwhm=1.2)
-    im_gal = gal.drawImage(nx=32, ny=32, scale=0.2)
-    igal = jgs.InterpolatedImage(
-        im_gal, gsparams=jgs.GSParams(minimum_fft_size=128, maximum_fft_size=128)
-    )
-
-    def f():
-        return igal.drawImage(nx=32, ny=32, scale=0.2)
-
-    benchmark(lambda: jax.jit(f)().array.block_until_ready())
-
-
-def test_benchmarks_interpolated_image_jit_run(benchmark):
+@pytest.mark.parametrize("kind", ["compile", "run"])
+def test_benchmarks_interpolated_image(benchmark, kind):
     gal = jgs.Gaussian(fwhm=1.2)
     im_gal = gal.drawImage(nx=32, ny=32, scale=0.2)
     igal = jgs.InterpolatedImage(
@@ -28,8 +22,106 @@ def test_benchmarks_interpolated_image_jit_run(benchmark):
 
     jitf = jax.jit(f)
 
-    # run once to compile
-    jitf().array.block_until_ready()
+    if kind == "compile":
+        benchmark(lambda: jitf().array.block_until_ready())
+    elif kind == "run":
+        jitf().array.block_until_ready()
+        benchmark(lambda: jitf().array.block_until_ready())
+    else:
+        raise ValueError(f"kind={kind} not recognized")
 
-    # now benchmark
-    benchmark(lambda: jitf().array.block_until_ready())
+
+@partial(jax.jit, static_argnames=("nk",))
+def _metacal_jax_galsim_render(im, psf, g1, target_psf, scale, nk):
+    prepsf_im = jgs.Convolve(im, jgs.Deconvolve(psf))
+    prepsf_im = prepsf_im.shear(g1=g1, g2=0.0)
+
+    prof = jgs.Convolve(
+        prepsf_im,
+        target_psf,
+        gsparams=jgs.GSParams(minimum_fft_size=nk, maximum_fft_size=nk),
+    )
+
+    return prof.drawImage(
+        nx=33,
+        ny=33,
+        scale=scale,
+        method="no_pixel",
+    ).array.astype(np.float64)
+
+
+@partial(jax.jit, static_argnames=("nk",))
+def _metacal_jax_galsim(im, psf, nse_im, scale, target_fwhm, g1, nk):
+    iim = jgs.InterpolatedImage(jgs.ImageD(im), scale=scale, x_interpolant="lanczos15")
+    ipsf = jgs.InterpolatedImage(
+        jgs.ImageD(psf), scale=scale, x_interpolant="lanczos15"
+    )
+    inse = jgs.InterpolatedImage(
+        jgs.ImageD(jnp.rot90(nse_im, 1)), scale=scale, x_interpolant="lanczos15"
+    )
+
+    target_psf = jgs.Gaussian(fwhm=target_fwhm)
+
+    sim = _metacal_jax_galsim_render(iim, ipsf, g1, target_psf, scale, nk)
+
+    snse = _metacal_jax_galsim_render(inse, ipsf, g1, target_psf, scale, nk)
+
+    return sim + jnp.rot90(snse, 3)
+
+
+@pytest.mark.parametrize("kind", ["compile", "run"])
+def test_benchmarks_metacal(benchmark, kind):
+    seed = 42
+    hlr = 0.5
+    fwhm = 0.9
+    scale = 0.2
+    g1 = 0.01
+    target_fwhm = 1.0
+    nse = 0.001
+
+    rng = np.random.RandomState(seed)
+
+    im = (
+        _galsim.Convolve(
+            _galsim.Exponential(half_light_radius=hlr),
+            _galsim.Gaussian(fwhm=fwhm),
+        )
+        .drawImage(
+            nx=33,
+            ny=33,
+            scale=scale,
+        )
+        .array.astype(np.float64)
+    )
+
+    psf = (
+        _galsim.Gaussian(fwhm=fwhm)
+        .drawImage(
+            nx=33,
+            ny=33,
+            scale=scale,
+        )
+        .array.astype(np.float64)
+    )
+
+    nse_im = rng.normal(size=im.shape, scale=nse)
+    im += rng.normal(size=im.shape, scale=nse)
+
+    def _run():
+        return _metacal_jax_galsim(
+            im.copy(),
+            psf.copy(),
+            nse_im.copy(),
+            scale,
+            target_fwhm,
+            g1,
+            128,
+        )
+
+    if kind == "compile":
+        benchmark(lambda: _run().array.block_until_ready())
+    elif kind == "run":
+        _run().array.block_until_ready()
+        benchmark(lambda: _run().array.block_until_ready())
+    else:
+        raise ValueError(f"kind={kind} not recognized")
