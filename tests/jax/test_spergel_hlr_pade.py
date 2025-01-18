9a57fc679aaa4b9081937124e4b551feb989f829
beckermr <becker.mr@gmail.com>
2025-01-18 05:24:26 -0600

    style: pre the commit

--- tests/jax/test_spergel_hlr_pade.py
+++ tests/jax/test_spergel_hlr_pade.py
@@ -5,12 +5,16 @@ from jax_galsim.core.testing import time_code_block
 from jax_galsim.spergel import _spergel_hlr_pade, calculateFluxRadius
 
 
-@pytest.mark.parametrize("nu", [-0.85, -0.6, -0.5, 0.0, 0.1, 0.5, 1.0, 1.1, 1.5, 2, 2.7, 4.0])
+@pytest.mark.parametrize(
+    "nu", [-0.85, -0.6, -0.5, 0.0, 0.1, 0.5, 1.0, 1.1, 1.5, 2, 2.7, 4.0]
+)
 def test_spergel_hlr_pade_correct(nu):
     np.testing.assert_allclose(_spergel_hlr_pade(nu), calculateFluxRadius(0.5, nu))
 
 
-@pytest.mark.parametrize("nu", [-0.85, -0.6, -0.5, 0.0, 0.1, 0.5, 1.0, 1.1, 1.5, 2, 2.7, 4.0])
+@pytest.mark.parametrize(
+    "nu", [-0.85, -0.6, -0.5, 0.0, 0.1, 0.5, 1.0, 1.1, 1.5, 2, 2.7, 4.0]
+)
 def test_spergel_hlr_pade_time(nu):
     print("\n", end="", flush=True)
 
