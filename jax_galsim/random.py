5945ad10a29a33418eff5eb1732e23b89f1c0815
beckermr <becker.mr@gmail.com>
2025-01-30 14:44:51 -0600

    fix: cannot unroll discard since n is not static

--- jax_galsim/random.py
+++ jax_galsim/random.py
@@ -174,7 +174,7 @@ class BaseDeviate:
             key, _ = jrandom.split(key)
             return key
 
-        return jax.lax.fori_loop(0, n, __discard, key, unroll=True)
+        return jax.lax.fori_loop(0, n, __discard, key)
 
     @implements(
         _galsim.BaseDeviate.raw,
