bb604f385a76514ff1aaabcd1985f114b93266d2
EiffL <fr.eiffel@gmail.com>
2026-02-06 22:51:53 +0100

    fix wcs

--- jax_galsim/fitswcs.py
+++ jax_galsim/fitswcs.py
@@ -834,16 +834,28 @@ class GSFitsWCS(CelestialWCS):
             and jnp.array_equal(self.cd, other.cd)
             and self.center == other.center
             and (
-                jnp.array_equal(self.pv, other.pv)
-                or (self.pv is None and other.pv is None)
+                (self.pv is None and other.pv is None)
+                or (
+                    self.pv is not None
+                    and other.pv is not None
+                    and jnp.array_equal(self.pv, other.pv)
+                )
             )
             and (
-                jnp.array_equal(self.ab, other.ab)
-                or (self.ab is None and other.ab is None)
+                (self.ab is None and other.ab is None)
+                or (
+                    self.ab is not None
+                    and other.ab is not None
+                    and jnp.array_equal(self.ab, other.ab)
+                )
             )
             and (
-                jnp.array_equal(self.abp, other.abp)
-                or (self.abp is None and other.abp is None)
+                (self.abp is None and other.abp is None)
+                or (
+                    self.abp is not None
+                    and other.abp is not None
+                    and jnp.array_equal(self.abp, other.abp)
+                )
             )
         )
 
