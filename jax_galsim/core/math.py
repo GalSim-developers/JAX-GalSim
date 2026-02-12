6b3da10d1cd05c70997d8569af6ffeedbc32e4a5
Matthew R. Becker <beckermr@users.noreply.github.com>
2026-02-12 05:54:13 -0600

    Apply suggestion from @beckermr

--- jax_galsim/core/math.py
+++ jax_galsim/core/math.py
@@ -4,7 +4,7 @@ import jax.numpy as jnp
 
 @jax.jit
 def safe_sqrt(x):
-    """Numerically safe sqrt operation with zero derivative as zero."""
+    """Numerically safe sqrt operation with zero derivative at zero."""
     msk = x > 0
     x_msk = jnp.where(msk, x, 1.0)
     return jnp.where(msk, jnp.sqrt(x_msk), 0.0)
