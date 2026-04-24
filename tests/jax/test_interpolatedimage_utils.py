15eac76b94fdda42dc1d3f95571e6c5375ca861c
Matthew R. Becker <beckermr@users.noreply.github.com>
2026-04-24 12:31:38 -0500

    Update tests/jax/test_interpolatedimage_utils.py
    Co-authored-by: Ismael Mendoza <11745764+ismael-mendoza@users.noreply.github.com>

--- tests/jax/test_interpolatedimage_utils.py
+++ tests/jax/test_interpolatedimage_utils.py
@@ -234,6 +234,7 @@ def test_interpolatedimage_utils_comp_to_galsim(
     )
 
     np.testing.assert_allclose(jgii.stepk, gii.stepk, rtol=0, atol=1e-6)
+    #FIXME: match maxk
     np.testing.assert_allclose(jgii.maxk, gii.maxk, rtol=0.5, atol=0)
     assert jgii.maxk >= gii.maxk
     kxvals = [
