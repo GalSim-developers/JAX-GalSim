64bde2b662eaf5338f918b39cf79b824eee0070d
Ismael Mendoza <imendoza@umich.edu>
2026-05-12 14:36:04 -0400

    feat: improve timing test with good size cut on catalog

--- notebooks/timing_tests_cpu.py
+++ notebooks/timing_tests_cpu.py
@@ -4,15 +4,17 @@ import os
 
 os.environ["JAX_ENABLE_X64"] = "True"
 import time
+from pathlib import Path
 
 import galsim
 import matplotlib.pyplot as plt
 import numpy as np
 from astropy.table import Table
 from draw_scripts import (
+    _format_column_to_dict,
     draw_galsim,
     draw_jgs_scan_stamps,
-    draw_jgs_vmap_stamps,
+    get_bd_galsim,
     get_default_lsst_background,
     get_one_full_sample,
 )
@@ -22,9 +24,9 @@ from tqdm import tqdm
 
 import jax_galsim as jgs
 
-IMAGE_SLEN = 200
+IMAGE_SLEN = 250
 MAX_N_GALS = 120
-STAMP_SLEN = 80
+STAMP_SLEN = 61
 BACKGROUND = get_default_lsst_background()  # for residual assessment
 FFT_SIZE = 128
 
@@ -46,12 +48,35 @@ def _prepare_catalog(
     return fcat
 
 
-def _add_results_to_psf(gs_arr, jgs_np_arr, t_galsim, t_jgalsim, ii, pdf):
+def _cut_on_good_sizes(*, cat, psf, max_good_size: int, overwrite: bool = False):
+    if Path("good_sizes.npy").exists() and not overwrite:
+        print("INFO: Loading good sizes from file...")
+        _good_sizes = np.load("good_sizes.npy")
+    else:
+        print("INFO: Computing good sizes for catalog")
+        # takes < 1 min
+        _good_sizes = []
+        for ii in tqdm(range(len(cat)), desc="Getting good sizes for cut..."):
+            gal = get_bd_galsim(**_format_column_to_dict(cat[ii]), psf=psf)
+            _good_size = gal.getGoodImageSize(0.2)
+            _good_sizes.append(_good_size)
+
+        _good_sizes = np.array(_good_sizes)
+        np.save("good_sizes.npy", _good_sizes)
+
+    mask = _good_sizes < max_good_size
+    return cat[mask]
+
+
+def _add_results_to_pdf(gs_arr, jgs_np_arr, t_galsim, t_jgalsim, ii, pdf):
 
     vmin = min(gs_arr.min(), jgs_np_arr.min())
     vmax = max(gs_arr.max(), jgs_np_arr.max())
 
     residual = gs_arr - jgs_np_arr
+    # make sure colorbar for residual is symmetric
+    res_vmin = -max(abs(residual.min()), abs(residual.max()))
+    res_vmax = max(abs(residual.min()), abs(residual.max()))
 
     fig, axes = plt.subplots(1, 3, figsize=(18, 6))
     fig.suptitle(
@@ -69,7 +94,9 @@ def _add_results_to_psf(gs_arr, jgs_np_arr, t_galsim, t_jgalsim, ii, pdf):
     axes[1].set_title("JAX-GalSim")
     fig.colorbar(im1, ax=axes[1])
 
-    im2 = axes[2].imshow(residual, origin="lower", cmap="RdBu_r")
+    im2 = axes[2].imshow(
+        residual, origin="lower", cmap="RdBu_r", vmin=res_vmin, vmax=res_vmax
+    )
     axes[2].set_title("Residual (GalSim - JAX-GalSim)")
     fig.colorbar(im2, ax=axes[2])
 
@@ -85,14 +112,23 @@ def main():
     catsim_file = "../../../Downloads/catsim/OneDegSq.fits"
 
     cat = _prepare_catalog(catsim_file)
+    cat = _cut_on_good_sizes(
+        cat=cat, psf=psf, max_good_size=STAMP_SLEN, overwrite=False
+    )
+
+    print("INFO: Catalog prepared with {} galaxies after cuts.".format(len(cat)))
 
     times_galsim = []
     times_jgalsim = []
 
+    # get mean_sources
+
     # timing results average over 100 samples
     rkeys = random.split(random.PRNGKey(42), 100)
     with PdfPages("residuals.pdf") as pdf:
-        for ii, rkey in tqdm(enumerate(rkeys)):
+        for ii, rkey in tqdm(
+            enumerate(rkeys), total=len(rkeys), desc="Timing galsim vs jax-galsim..."
+        ):
             # sample in numpy
             sample, n = get_one_full_sample(
                 rkey, cat=cat, ilen=IMAGE_SLEN, max_n_gals=MAX_N_GALS
@@ -110,7 +146,11 @@ def main():
 
             t1 = time.time()
             gs_arr = draw_galsim(
-                sample, n, psf=psf, ilen=IMAGE_SLEN, max_n_gals=MAX_N_GALS
+                sample,
+                n,
+                psf=psf,
+                ilen=IMAGE_SLEN,
+                max_n_gals=MAX_N_GALS,
             )
             t2 = time.time()
             t_galsim = t2 - t1
@@ -134,9 +174,18 @@ def main():
             times_jgalsim.append(t_jgalsim)
 
             # save residual images to a multipage pdf for inspection
-            _add_results_to_psf(gs_arr, np.array(jgs_arr), t_galsim, t_jgalsim, ii, pdf)
+            _add_results_to_pdf(gs_arr, np.array(jgs_arr), t_galsim, t_jgalsim, ii, pdf)
 
-    print(np.median(times_galsim), np.median(times_jgalsim))
+    # print summary timing results
+    print(f"Median time (per image) for GalSim: {np.median(times_galsim):.3f} seconds")
+    print(f"Average time (per image) for GalSim: {np.mean(times_galsim):.3f} seconds")
+
+    print(
+        f"Median time (per image) for JAX-GalSim: {np.median(times_jgalsim):.3f} seconds"
+    )
+    print(
+        f"Average time (per image) for JAX-GalSim: {np.mean(times_jgalsim):.3f} seconds"
+    )
 
 
 if __name__ == "__main__":
