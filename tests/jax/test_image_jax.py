6b13a691789b9cf7c2fbba45f482b15aeeb6c8a1
beckermr <becker.mr@gmail.com>
2026-05-12 17:07:36 -0500

    test: clean up code I do not need

--- tests/jax/test_image_jax.py
+++ tests/jax/test_image_jax.py
@@ -6,8 +6,5 @@ import jax_galsim
 
 def test_image_jax_view_raises():
     im = jax_galsim.ImageD(np.arange(20).reshape(4, 5))
-    with pytest.raises(NotImplementedError) as exc:
+    with pytest.raises(NotImplementedError):
         im.view()
-    # for reasons I do not follow, pytest is not failing if I check
-    # the wrong exception type
-    assert exc.value.__class__ is NotImplementedError
