d8296c38cde0212476f392fc2f5922c9f3dc4d74
beckermr <becker.mr@gmail.com>
2026-05-12 16:56:31 -0500

    test: add test to assert .view raises for images

---
+++ tests/jax/test_image_jax.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pytest
+
+import jax_galsim
+
+
+def test_image_jax_view_raises():
+    im = jax_galsim.ImageD(np.arange(20).reshape(4, 5))
+    with pytest.raises(RuntimeError):
+        im.view()
