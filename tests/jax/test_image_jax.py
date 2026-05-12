10efae528cd7abe7248ac275e7a8cb2f80cf0299
beckermr <becker.mr@gmail.com>
2026-05-12 17:05:06 -0500

    fix: check proper exception typer

--- tests/jax/test_image_jax.py
+++ tests/jax/test_image_jax.py
@@ -6,5 +6,8 @@ import jax_galsim
 
 def test_image_jax_view_raises():
     im = jax_galsim.ImageD(np.arange(20).reshape(4, 5))
-    with pytest.raises(RuntimeError):
+    with pytest.raises(NotImplementedError) as exc:
         im.view()
+    # for reasons I do not follow, pytest is not failing if I check
+    # the wrong exception type
+    assert exc.value.__class__ is NotImplementedError
