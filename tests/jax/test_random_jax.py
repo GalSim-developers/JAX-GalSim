df2a3d4a31a077edda88cc652a5f9ce798ef7641
beckermr <becker.mr@gmail.com>
2026-05-15 12:39:21 -0500

    fix: more tests

--- tests/jax/test_random_jax.py
+++ tests/jax/test_random_jax.py
@@ -13,10 +13,18 @@ def test_random_jax_gaussian_pos_sigma_jit():
         _make_gauss(-1.0)
 
     @jax.jit
-    def _make_gauss(sigma):
+    def _make_gauss_again(sigma):
         return jax_galsim.GaussianDeviate(seed=10, sigma=sigma)
 
-    _make_gauss(1.0)
+    _make_gauss_again(1.0)
 
     with pytest.raises(Exception):
-        _make_gauss(-1)
+        _make_gauss_again(-1)
+
+    def _make_gauss_again_again(sigma):
+        return jax_galsim.GaussianDeviate(seed=10, sigma=sigma)
+
+    _make_gauss_again_again(1.0)
+
+    with pytest.raises(Exception):
+        jax.jit(_make_gauss_again_again)(-1)
