6c8e044cc3537314822293df746a43063a6462c5
Ismael Mendoza <imendoza@umich.edu>
2026-07-01 09:49:48 -0400

    small improvement so that we only time the correct thing

--- scripts/timing_tests_phot.py
+++ scripts/timing_tests_phot.py
@@ -11,7 +11,7 @@ from functools import partial
 import galsim as _galsim
 import jax.random as jrng
 import numpy as np
-from jax import block_until_ready, jit, transfer_guard, vmap
+from jax import block_until_ready, device_put, jit, transfer_guard, vmap
 
 import jax_galsim as jgs
 from jax_galsim.photon_array import fixed_photon_array_size
@@ -20,6 +20,9 @@ from jax_galsim.photon_array import fixed_photon_array_size
 def main():
     rng_key = jrng.key(42)
     n_obj = 1000
+    k1, k2 = jrng.split(rng_key)
+    pkeys = jrng.split(k1, n_obj)
+    skeys = jrng.split(k2, n_obj)
 
     psf = _galsim.Gaussian(half_light_radius=0.7, flux=1.0)
     xpsf = jgs.Gaussian(half_light_radius=0.7, flux=1.0)
@@ -27,25 +30,30 @@ def main():
     # prepare jax_galsim draw function
     _draw_fnc = jit(partial(_generate_image_phot_jgs, psf=xpsf, n_obj=n_obj))
 
+    # get all parameters for both galsim and jgs
+    params = vmap(_generate_random_params)(pkeys)
+    params = [np.array(p) for p in params]
+
     # time galsim
     t1 = time.time()
-    _ = _generate_image_phot_galsim(rng_key, psf=psf, n_obj=n_obj)
+    _ = _generate_image_phot_galsim(params, psf=psf, n_obj=n_obj)
     t2 = time.time()
     t_galsim = t2 - t1
 
     # time jax-galsim
-    # compilation
-    _ = block_until_ready(_draw_fnc(rng_key))
+    # compilation and transfer
+    params_jax = device_put(params)
+    _ = block_until_ready(_draw_fnc(skeys, params_jax))
 
     # timing
     t1 = time.time()
     with transfer_guard("disallow"):
-        _ = block_until_ready(_draw_fnc(rng_key))
+        _ = block_until_ready(_draw_fnc(skeys, params_jax))
     t2 = time.time()
     t_jgs = t2 - t1
 
-    print(f"Time GalSim: {t_galsim:.4f}")
-    print(f"Time JAX-GalSim: {t_jgs:.4f}")
+    print(f"Time GalSim (seconds): {t_galsim:.4f}")
+    print(f"Time JAX-GalSim (seconds): {t_jgs:.4f}")
 
 
 def _generate_random_params(rng_key):
@@ -63,19 +71,20 @@ def _generate_random_params(rng_key):
     rng_key, use_key = jrng.split(rng_key)
     dy = jrng.uniform(use_key, minval=-10, maxval=10)
     rng_key, use_key = jrng.split(rng_key)
-
-    return (
-        flux,
-        hlr,
-        g1,
-        g2,
-        dx,
-        dy,
-    ), use_key
+    return flux, hlr, g1, g2, dx, dy
 
 
-def _generate_image_one_phot_jgs(rng_key, psf):
-    (flux, hlr, g1, g2, dx, dy), use_key = _generate_random_params(rng_key)
+def _generate_image_phot_jgs(rng_keys, params, psf, n_obj):
+    with fixed_photon_array_size(1000):
+        images = vmap(_generate_image_one_phot_jgs, in_axes=(0, 0, None))(
+            rng_keys, params, psf
+        )
+    assert images.array.shape[0] == n_obj
+    return images
+
+
+def _generate_image_one_phot_jgs(rng_key, params, psf):
+    flux, hlr, g1, g2, dx, dy = params
 
     return (
         jgs.Convolve(
@@ -89,36 +98,16 @@ def _generate_image_one_phot_jgs(rng_key, psf):
         )
         .withGSParams(minimum_fft_size=1024, maximum_fft_size=1024)
         .drawImage(
-            nx=200, ny=200, scale=0.2, method="phot", rng=jgs.BaseDeviate(use_key)
+            nx=200, ny=200, scale=0.2, method="phot", rng=jgs.BaseDeviate(rng_key)
         )
     )
 
 
-def _generate_image_phot_jgs(rng_key, psf, n_obj):
-    use_keys = jrng.split(rng_key, num=n_obj + 1)
-    rng_key = use_keys[0]
-    use_keys = use_keys[1:]
-
-    with fixed_photon_array_size(1000):
-        images = vmap(_generate_image_one_phot_jgs, in_axes=(0, None))(use_keys, psf)
-    assert images.array.shape[0] == n_obj
-    return images
-
-
-def _generate_image_phot_galsim(rng_key, psf, n_obj):
-    use_keys = jrng.split(rng_key, num=n_obj + 1)
-    rng_key = use_keys[0]
-    use_keys = use_keys[1:]
+def _generate_image_phot_galsim(params, psf, n_obj):
 
     all_out = []
-    for _ in range(n_obj):
-        (flux, hlr, g1, g2, dx, dy), _ = _generate_random_params(rng_key)
-        flux = flux.item()
-        hlr = hlr.item()
-        g1 = g1.item()
-        g2 = g2.item()
-        dx = dx.item()
-        dy = dy.item()
+    for ii in range(n_obj):
+        (flux, hlr, g1, g2, dx, dy) = [p[ii] for p in params]
         img = (
             _galsim.Convolve(
                 [
